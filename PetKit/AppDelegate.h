7f5c3627dffc75f3f0d3c2f851645fa34a717a2c
Danny.Xu <dannyxuprivate@163.com>
2016-10-20 21:34:53 +0800

    Clean code with basic DFUSDK usage.

---
+++ PetKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PetKit
+//
+//  Created by Danny.Xu on 2016/10/19.
+//  Copyright © 2016年 Danny.Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PetKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PetKit
+//
+//  Created by Danny.Xu on 2016/10/19.
+//  Copyright © 2016年 Danny.Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
